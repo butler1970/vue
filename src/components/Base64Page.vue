0e0225ade2c3f0bc04d4dde8aab408f7b54de6dd
Robert Butler <butler1970@gmail.com>
2023-07-29 21:26:04 -0700

    Added Base64Page.  Work in progress.

--- src/components/Base64Page.vue
+++ src/components/Base64Page.vue
@@ -1,62 +1,73 @@
 <template>
   <h1>Base64</h1>
 
-  <p>Welcome to the Base64 page.</p>
+  <p>Welcome to the Base64 page.  Below you will find helpers to base64 encode and decode a value.  The helpers use
+  the javascript functions <b>atob</b> and <b>btoa</b>.  For more information about these functions you can refer
+  to the following MDN documentation,
+    <a target="_blank" href="https://developer.mozilla.org/en-US/docs/Glossary/Base64">Javascript Base64</a>.
+    For more information on the purpose of Base64, you can refer to the Wikipedia article,
+    <a target="_blank" href="https://en.wikipedia.org/wiki/Base64">Base64</a>
+  </p>
 
-  <v-sheet :elevation="12">
+  <v-container>
+    <v-row no-gutters>
+      <v-col cols="12">
+        <v-sheet color="primary" class="pa-2 ma-2">
+          Base64 Encode
+        </v-sheet>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col cols="12" md="6">
+        <v-sheet color="secondary" class="pa-2 ma-2">
+          <v-text-field label="Value" v-model="base64Encode.input"></v-text-field>
+        </v-sheet>
+      </v-col>
+      <v-col cols="12" md="2">
+        <v-sheet color="secondary" class="pa-2 ma-2">
+          <v-btn @click="encode(base64Encode.input)">Encode</v-btn>
+        </v-sheet>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col cols="12" md="6">
+        <v-sheet color="secondary" class="pa-2 ma-2">
+          <v-textarea label="Result" v-model="base64Encode.result"></v-textarea>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 
-    <v-container class="container-with-label">
-      <div class="container-label">
-        <v-chip label>Base64 Encode</v-chip>
-      </div>
-      <v-row no-gutters>
-        <v-col cols="12" md="6">
-          <v-sheet color="secondary" class="pa-2 ma-2">
-            <v-text-field label="Value" v-model="base64Encode.input"></v-text-field>
-          </v-sheet>
-        </v-col>
-        <v-col cols="12" md="2">
-          <v-sheet color="secondary" class="pa-2 ma-2">
-            <v-btn  @click="encode(base64Encode.input)">Encode</v-btn>
-          </v-sheet>
-        </v-col>
-      </v-row>
-      <v-row no-gutters>
-        <v-col cols="12" md="6">
-          <v-sheet color="secondary" class="pa-2 ma-2">
-            <v-textarea label="Result" v-model="base64Encode.result"></v-textarea>
-          </v-sheet>
-        </v-col>
-      </v-row>
-    </v-container>
 
+  <v-container>
+    <v-row no-gutters>
+      <v-col cols="12">
+        <v-sheet color="primary" class="pa-2 ma-2">
+          Base64 Encode
+        </v-sheet>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col cols="12" md="6">
+        <v-sheet color="secondary" class="pa-2 ma-2">
+          <v-textarea label="Value" v-model="base64Decode.input"></v-textarea>
+        </v-sheet>
+      </v-col>
+      <v-col cols="12" md="2">
+        <v-sheet color="secondary" class="pa-2 ma-2">
+          <v-btn @click="decode(base64Decode.input)">Decode</v-btn>
+        </v-sheet>
+      </v-col>
+    </v-row>
+    <v-row no-gutters>
+      <v-col cols="12" md="6">
+        <v-sheet color="secondary" class="pa-2 ma-2">
+          <v-text-field label="Result" v-model="base64Decode.result"></v-text-field>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 
-    <v-container class="container-with-label">
-      <div class="container-label">
-        <v-chip label>Base64 Decode</v-chip>
-      </div>
-      <v-row no-gutters>
-        <v-col cols="12" md="6">
-          <v-sheet color="secondary" class="pa-2 ma-2">
-            <v-textarea label="Value" v-model="base64Decode.input"></v-textarea>
-          </v-sheet>
-        </v-col>
-        <v-col cols="12" md="2">
-          <v-sheet color="secondary" class="pa-2 ma-2">
-            <v-btn  @click="decode(base64Decode.input)">Decode</v-btn>
-          </v-sheet>
-        </v-col>
-      </v-row>
-      <v-row no-gutters>
-        <v-col cols="12" md="6">
-          <v-sheet color="secondary" class="pa-2 ma-2">
-            <v-text-field label="Result" v-model="base64Decode.result"></v-text-field>
-          </v-sheet>
-        </v-col>
-      </v-row>
-    </v-container>
-
-  </v-sheet>
 </template>
 
 <script>
@@ -87,17 +98,6 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-.container-label {
-  text-align: left;
-  margin-top: -18px;
-  margin-left: -18px;
-}
-
-.container-with-label {
-  border-top-width: 1px;
-  border-top-style: solid;
-}
 
 p {
   text-align: justify;
