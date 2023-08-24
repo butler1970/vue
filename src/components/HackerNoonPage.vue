8c952c068ff5e15afa194948fc49dd8f79912401
Robert Butler <butler1970@gmail.com>
2023-08-24 08:31:09 -0700

    Added social media sharing capability.

--- src/components/HackerNoonPage.vue
+++ src/components/HackerNoonPage.vue
@@ -1,10 +1,10 @@
 <template>
   <h1>HackerNoon</h1>
 
-  <p>Welcome to the HackerNoon page.  As an exercise to consume an RSS feed from a site and render it using VueJS
-  I have selected a site which I ran across recently that has some interesting articles on topics across a broad
-  spectrum of technology niches such as crypto currency, artificial intelligence, programming, machine learning,
-    gaming, etc.  If these articles are of interest to you, please visit
+  <p>Welcome to the HackerNoon page. As an exercise to consume an RSS feed from a site and render it using VueJS
+    I have selected a site which I ran across recently that has some interesting articles on topics across a broad
+    spectrum of technology niches such as crypto currency, artificial intelligence, programming, machine learning,
+    gaming, etc. If these articles are of interest to you, please visit
     <a target="_blank" href="https://hackernoon.com">HackerNoon.</a>
   </p>
 
@@ -19,19 +19,13 @@
           <v-col cols="12" md="2">
             <v-sheet color="primary" class="pa-2 ma-2">Title</v-sheet>
           </v-col>
-          <v-col cols="12" md="4">
+          <v-col cols="12" md="6">
             <v-sheet color="secondary" class="pa-2 ma-2 text-left">{{ item.title }}</v-sheet>
           </v-col>
-          <v-col cols="12" md="2" v-if="canShare">
-            <v-sheet>
-              <v-btn @click="shareArticle(item)">Share</v-btn>
+          <v-col cols="12" md="4">
+            <v-sheet color="secondary" class="pa-2 ma-2 float-right">
+              <v-img :src="getImageUrl(item.media.thumbnail.url)" width="300"/>
             </v-sheet>
-          </v-col>
-          <v-col cols="12" md="4" v-if="canShare">
-            <v-sheet color="secondary" class="pa-2 ma-2"><v-img :src="getImageUrl(item.media.thumbnail.url)" width="300"/></v-sheet>
-          </v-col>
-          <v-col cols="12" md="6" v-if="!canShare">
-            <v-sheet color="secondary" class="pa-2 ma-2"><v-img :src="getImageUrl(item.media.thumbnail.url)" width="300"/></v-sheet>
           </v-col>
         </v-row>
         <v-row>
@@ -55,7 +49,9 @@
             <v-sheet color="primary" class="pa-2 ma-2">Link</v-sheet>
           </v-col>
           <v-col cols="12" md="10">
-            <v-sheet color="secondary" class="pa-2 ma-2 text-left"><a target="_blank" :href="item.link">{{ item.link }}</a></v-sheet>
+            <v-sheet color="secondary" class="pa-2 ma-2 text-left"><a target="_blank" :href="item.link">{{
+                item.link
+              }}</a></v-sheet>
           </v-col>
         </v-row>
         <v-row>
@@ -64,6 +60,18 @@
           </v-col>
           <v-col cols="12" md="10">
             <v-sheet color="secondary" class="pa-2 ma-2 text-left">{{ item.category.join(', ') }}</v-sheet>
+          </v-col>
+        </v-row>
+        <v-row>
+          <v-col cols="12" md="2">
+            <v-sheet color="primary" class="pa-2 ma-2">Share</v-sheet>
+          </v-col>
+          <v-col cols="12" md="10">
+            <v-sheet color="secondary" class="pa-2 ma-2 float-right">
+              <v-btn @click="shareOnFacebook(item)"><v-icon>mdi-facebook</v-icon></v-btn>
+              <v-btn @click="shareOnTwitter(item)"><v-icon>mdi-twitter</v-icon></v-btn>
+              <v-btn @click="shareWithWebApi(item)" v-if="canShare"><v-icon class="fas fa-share"></v-icon></v-btn>
+            </v-sheet>
           </v-col>
         </v-row>
       </v-sheet>
@@ -79,7 +87,7 @@
 <script>
 import {resolveApiUrl} from "@/util";
 
-const { parse } = require('rss-to-json');
+const {parse} = require('rss-to-json');
 
 export default {
   name: 'HackerNoonPage',
@@ -96,7 +104,7 @@ export default {
   },
   created() {
     this.load();
-    this.canShare = navigator.share !== undefined;
+    this.canShare = true; //navigator.share !== undefined;
   },
   watch: {
     // feed(newValue) {
@@ -110,7 +118,7 @@ export default {
     getImageUrl(input) {
       return input.replace(/https:\/\/hackernoon.com\//g, '');
     },
-    shareArticle(input) {
+    shareWithWebApi(input) {
       navigator.share({
         title: input.title,
         text: 'Check it out!',
@@ -124,6 +132,12 @@ export default {
             this.sharedToast.message = error;
             this.sharedToast.show = true;
           });
+    },
+    shareOnFacebook(input) {
+      window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(input.link)}`, '_blank');
+    },
+    shareOnTwitter(input) {
+      window.open(`https://twitter.com/intent/tweet?url=${encodeURIComponent(input.link)}&text=${encodeURIComponent('Check this out!')}`, '_blank');
     }
   }
 }
